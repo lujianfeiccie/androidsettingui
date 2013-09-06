11231f32903928b7a143e131c3d8671ee9be1986
Jacky <lujianfeiccie2012@163.com>
2013-09-06 22:23:50 +0800

    first commit

---
+++ src/com/lujianfei/ui/demo/QQSettingLayoutDemoActivity.java
@@ -0,0 +1,20 @@
+package com.lujianfei.ui.demo;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * 
+ * @author genius
+ * 更多精彩请关注我的CSDN博客:http://blog.csdn.net/geniuseoe2012
+ */
+
+
+public class QQSettingLayoutDemoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_layout);
+    }
+}
